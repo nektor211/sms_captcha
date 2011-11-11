cffc23adb584a7d6224f9ec7560930cf0fe99ade
Pavel Taufer <paja.taufer@gmail.com>
2011-11-11 23:06:32 +0100

    tag

---
+++ tag.php
@@ -0,0 +1,33 @@
+<?php
+echo passthru("pwd");
+foreach ($_POST as $key=>$value) {
+  if(strstr($key, "____")){
+    $txt=substr_replace($key, ".txt", -4);
+    $file=fopen("samples/".$txt,"w");
+    fwrite($file, $value);
+    fclose($file);
+  }
+}
+
+echo '<form action="tag.php" method="post">';
+$d = dir("samples");
+
+while (false !== ($entry = $d->read())) {
+  if(strstr($entry, ".jpg")){
+    #echo $entry."\n";
+    $txt=substr_replace($entry, "txt", -3);
+    $pod=substr_replace($entry, "____", -4);
+    //echo $txt."\n";
+    if(file_exists("samples/$txt")){
+	  $lines=file("samples/$txt");
+	  $data=chop($lines[0]);
+	}else{
+	  $data="";
+	}
+	//echo $data."\n";
+	echo "<image src=\"samples/$entry\" /><br>\n";
+	echo "<input type=\"text\" name=\"$pod\" value=\"$data\"><br>";
+  }
+}echo "<input type=\"submit\"></form>";
+$d->close();
+?>
