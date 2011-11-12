42968c27d528fd64828b90c022c91ac06394caf3
Your Name <you@example.com>
2011-11-13 00:34:34 +0100

    unrollF - only unroll features, no tags

---
+++ unrollF.php
@@ -0,0 +1,65 @@
+<?php 
+	
+	
+	$myFILEout = fopen("splits/features.txt", "w");
+	
+	$d = dir("splits/c");
+
+	while(false != ($entry = $d->read()) ) {
+		
+	 if ((strstr($entry, "c")) && (strstr($entry, ".txt"))) {
+			$myFILEin = fopen("splits/c/$entry", "r");
+			while (!feof($myFILEin)) {
+				$c = fgetc($myFILEin);
+				if($c == 'r') {
+					fwrite($myFILEout, " 1");
+				}
+				else if(($c == 'w') || ($c == 'b') ) {
+					fwrite($myFILEout, " 0");
+				}
+			}
+			fwrite($myFILEout, "\n");
+
+			fclose($myFILEin);
+		}
+	}
+	//fclose($myFILEout);
+/*
+	$myFILEoutT = fopen("splits/tags.txt", "w"); 
+	$d = dir("splits/txt");
+	while(false != ($entry = $d->read())) {
+		if ((strstr($entry, "s")) && (strstr($entry, ".txt"))){
+			$myFILEin = fopen("splits/txt/$entry", "r");
+			if (!feof($myFILEin)) $i = fgetc($myFILEin);
+			fwrite($myFILEoutT, "$i\n");
+			fclose($myFILEin);		
+		}	
+	}
+
+
+
+	fclose($myFILEout);
+	fclose($myFILEoutT);
+
+
+	$fileF = fopen("splits/features.txt", "r");
+	$fileT = fopen("splits/tags.txt", "r");
+	$FILEout = fopen("data.txt", "w");
+
+	while (!feof($fileT) && !feof($fileF)) {
+		$line = trim(fgets($fileF));
+		$tag = trim(fgets($fileT));
+		if ($tag != 0) {
+			fwrite($FILEout, $line." ".$tag."\n");
+		}
+	}
+	fclose($fileF);
+	fclose($fileT);
+	fclose($FILEout);
+*/
+
+fclose($myFILEout);
+
+
+
+?>
