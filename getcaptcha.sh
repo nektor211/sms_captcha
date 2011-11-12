f48a8c7758190e3ca80188b7981d6670b261a33c
Pavel Taufer <paja.taufer@gmail.com>
2011-11-12 11:53:23 +0100

    gc - stahuje jen captcha.jpg ted

--- getcaptcha.sh
+++ getcaptcha.sh
@@ -1,7 +1,8 @@
 url=http://sms.t-zones.cz/open/captcha.jpg
 #wpar=" --cookies=on --keep-session-cookies --load-cookies=${dir}cookie.txt --save-cookies=${dir}cookie.txt "
+wget $wpar $url
 for x in `seq 0 2` 
 do
-  wget $wpar $url -O samples/${x}.jpg
+#  wget $wpar $url -O samples/${x}.jpg
   ./nap.pl 200
 done
